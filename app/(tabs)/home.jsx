9e6ba6f168038e127c94e81bb2d86c57823f5625
Karan Panchal <karanhpanchal09@gmail.com>
2024-08-04 15:33:26 +0530

    Crash App resolved

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,16 +1,26 @@
-import { View, Text, FlatList, Image, RefreshControl, Alert } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
-  const { data:posts, refetch } = useAppwrite(getAllPosts);
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
+  // console.log(latestPosts);
   const [refreshing, setRefreshing] = useState(false);
   const onRefresh = async () => {
     setRefreshing(true);
@@ -23,11 +33,8 @@ const Home = () => {
     <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={posts}
-        // data={[]}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <VideoCard video={item}/>
-        )}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6 ">
@@ -52,8 +59,7 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
+import { getUserPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { icons } from "../../constants";
 
 const Profile = () => {
+  const { user, setUser, setIsLogged } = useGlobalContext();
+  console.log(">>>>>>>>>>>>>"+user.$id);
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+  console.log("////////////"+ posts.$id);
+
+  const logout = () => {
+
+  }
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+        <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            // creator={item.creator.username}
+            // avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+              <TouchableOpacity
+              className="w-full items-end mb-10" onPress={logout}>
+                <Image source={icons.logout} resizeMode="contain" className="w-6 h-6"
+                />
+              </TouchableOpacity>
+              <View className="w-16 h-16 borde border-secondary rounded-lg justify-center items-center">
+              {/* <Image source={{uri:avatar}} /> */}
+              </View>
+        
+            </View>
+          </>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Profile
-
-const styles = StyleSheet.create({})
+export default Profile;
